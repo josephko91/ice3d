1076c159fe0c665ccc763aaea17cd9137ca9424b
Joseph Ko <koseph123@gmail.com>
2025-03-24 11:20:46 -0600

    init ML data creation

---
+++ scripts/slurm/05-create-data.py
@@ -0,0 +1,6 @@
+'''
+This script calculates:
+(1) inputs/outputs necessary for supervised tabular ML 
+(2) simultaneously, this dataframe can be used for deep learning training
+- i.e., ID / output pairs 
+'''
